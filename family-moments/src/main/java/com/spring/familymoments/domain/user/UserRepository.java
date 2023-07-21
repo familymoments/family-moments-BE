d0fb155c7751f6b4fe033e367b3c8d5a7527f9b8
sonshn <sonshn@naver.com>
2023-07-21 14:09:08 +0900

    fix: getUser 메소드 수정

--- family-moments/src/main/java/com/spring/familymoments/domain/user/UserRepository.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/user/UserRepository.java
@@ -3,13 +3,16 @@ package com.spring.familymoments.domain.user;
 import com.spring.familymoments.domain.common.BaseEntity;
 import com.spring.familymoments.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByUuid(String uuid);
 
     // Optional<User> findByEmailAndStatus(String email, BaseEntity.Status status);
+    Optional<User> findById(String id);
     Optional<User> findByEmail(String email);
 }
