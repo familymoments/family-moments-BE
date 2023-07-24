481add4f0fca5814acd866d9e45df97d1225a300
sonshn <sonshn@naver.com>
2023-07-24 23:07:15 +0900

    feat: 이메일 중복확인 api

--- family-moments/src/main/java/com/spring/familymoments/domain/user/UserRepository.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/user/UserRepository.java
@@ -17,4 +17,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsById(String id);
+    boolean existsByEmail(String email);
 }
