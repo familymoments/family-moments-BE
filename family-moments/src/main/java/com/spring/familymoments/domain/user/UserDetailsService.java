9993edfb3311252791017d7354b945396db7831c
spacewalk00 <ju001217@gmail.com>
2023-07-25 20:36:20 +0900

    feat: 로그인, 로그아웃 api

---
+++ family-moments/src/main/java/com/spring/familymoments/domain/user/UserDetailsService.java
@@ -0,0 +1,24 @@
+package com.spring.familymoments.domain.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    /**
+     * springsecurity에서 유저를 찾는 메소드를 제공하는 UserDetailsService를 implements
+     * loadUserByUsername 메소드를 오버라이딩 하여 유저를 찾는 방법을 직접 지정함.
+     * 이 메소드의 username == '사용자 uuid'
+     *
+     */
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByUuid(username)
+                .orElseThrow(() -> new UsernameNotFoundException("[UserDetailService] 사용자를 찾을 수 없습니다."));
+    }
+
+}
