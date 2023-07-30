8b9bf99e30529c0c78f57a81a7da577fe14c6010
serak0310 <serak0310@naver.com>
2023-07-30 17:44:25 +0900

    feat: (메인)닉네임 및 가족 생성일 조회

--- family-moments/src/main/java/com/spring/familymoments/domain/family/FamilyService.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/family/FamilyService.java
@@ -4,6 +4,7 @@ import com.spring.familymoments.domain.common.UserFamilyRepository;
 import com.spring.familymoments.domain.common.entity.UserFamily;
 import com.spring.familymoments.domain.family.entity.Family;
 import com.spring.familymoments.domain.family.model.FamilyDto;
+import com.spring.familymoments.domain.family.model.GetFamilyCreatedNicknameRes;
 import com.spring.familymoments.domain.family.model.PostFamilyReq;
 import com.spring.familymoments.domain.family.model.PostFamilyRes;
 import com.spring.familymoments.domain.user.UserRepository;
@@ -15,6 +16,10 @@ import org.hibernate.Hibernate;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -59,7 +64,7 @@ public class FamilyService {
         // 2. 유저 가족 매핑 튜플 생성
         // 가족 외래키 생성
         Family preFamily = familyRepository.findById(savedFamily.getFamilyId())
-                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 사용자입니다."));
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 가족입니다."));
 
         // 유저가족 입력 객체 생성
         UserFamily userFamily = UserFamily.builder()
@@ -96,6 +101,25 @@ public class FamilyService {
                 .representImg(family.get().getRepresentImg())
                 .build();
 
+    }
+
+    // 닉네임 및 가족 생성일 조회
+    public GetFamilyCreatedNicknameRes getFamilyCreatedNickname(Long familyId, Long userId){
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 사용자입니다."));
+
+        Family family = familyRepository.findById(familyId)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 가족입니다."));
+
+        LocalDate createdAt = family.getCreatedAt().toLocalDate();
+        LocalDate now = LocalDate.now();
+
+        Period period = Period.between(createdAt, now);
+
+        String daysSinceCreation = String.valueOf(period.getDays()+1);
+
+        return new GetFamilyCreatedNicknameRes(user.getNickname(), daysSinceCreation);
     }
 
     public FamilyDto getFamilyByInviteCode(String inviteCode){
