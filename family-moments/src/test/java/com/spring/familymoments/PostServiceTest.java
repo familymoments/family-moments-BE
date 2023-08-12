4a761d256605e9dfab5db6d66488dbc033bc66ec
zzo3ozz <hhzzo@DESKTOP-VCOTNUF>
2023-08-12 12:43:21 +0900

    fix: token validation 추가

--- family-moments/src/test/java/com/spring/familymoments/PostServiceTest.java
+++ family-moments/src/test/java/com/spring/familymoments/PostServiceTest.java
@@ -14,35 +14,4 @@ import java.util.List;
 
 @SpringBootTest
 public class PostServiceTest {
-    @Autowired
-    PostRepository postRepository;
-    @Autowired
-    PostService postService;
-
-    @Test
-    void findOne() {
-        SinglePostRes post = postRepository.findByPostId(1, 5);
-
-        System.out.println(post.toString());
-    }
-
-    @Test
-    void countPostLove() {
-        postRepository.updateCountLove(5);
-    }
-
-    @Test
-    void multiplePost() throws BaseException {
-        List<AlbumRes> multiPostRes= postService.getAlbum(1, 15);
-
-        if(multiPostRes == null) {
-            System.out.print("Is null");
-        } else if(multiPostRes.isEmpty()){
-            System.out.print("Is empty");
-        }
-
-        for(AlbumRes res : multiPostRes) {
-            System.out.println("id : " + res.getPostId() + "\t" + res.getImg1());
-        }
-    }
 }
