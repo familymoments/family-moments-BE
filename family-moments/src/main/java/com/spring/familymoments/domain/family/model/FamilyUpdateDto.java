57e38f4341a5ad57063eef8f19ee8c4f7ba38a85
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-13 23:54:41 +0900

    refactor: 가족 정보 수정 dto에 validation 추가

--- family-moments/src/main/java/com/spring/familymoments/domain/family/model/FamilyUpdateDto.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/family/model/FamilyUpdateDto.java
@@ -2,11 +2,16 @@ package com.spring.familymoments.domain.family.model;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+
 @Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FamilyUpdateDto {
+
     private String owner;
+
+    @NotEmpty(message = "가족 이름을 입력해주세요.")
     private String familyName;
 }
