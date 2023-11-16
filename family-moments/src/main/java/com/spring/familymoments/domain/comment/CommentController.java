c811119dc6bda4f061bf4fc71a4ff1847c09e2a5
serak0310 <serak0310@naver.com>
2023-11-16 21:37:47 +0900

    feat: Comment - 로그인 일괄 처리 적용

--- family-moments/src/main/java/com/spring/familymoments/domain/comment/CommentController.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/comment/CommentController.java
@@ -2,6 +2,7 @@ package com.spring.familymoments.domain.comment;
 
 import com.spring.familymoments.config.BaseException;
 import com.spring.familymoments.config.BaseResponse;
+import com.spring.familymoments.config.NoAuthCheck;
 import com.spring.familymoments.domain.comment.model.GetCommentsRes;
 import com.spring.familymoments.domain.comment.model.PostCommentReq;
 import com.spring.familymoments.domain.user.AuthService;
@@ -31,17 +32,14 @@ public class CommentController {
      * @return BaseResponse<String>
      */
     @ResponseBody
+    @NoAuthCheck
     @PostMapping("")
     @Operation(summary = "댓글 생성", description = "댓글을 생성합니다.")
-    public BaseResponse<String> createComment(@AuthenticationPrincipal User user,
-                                              @RequestParam("postId") Long postId,
-                                              @RequestPart PostCommentReq postCommentReq,
-                                              @RequestHeader("X-AUTH-TOKEN") String requestAccessToken){
-
-        if (authService.validate(requestAccessToken)) { //유효한 사용자라 true가 반환됩니다 !!
-            return new BaseResponse<>(INVALID_JWT); //401 error : 유효한 사용자이지만, 토큰의 유효 기간이 만료됨.
-        }
-
+    public BaseResponse<String> createComment(
+            @AuthenticationPrincipal User user,
+            @RequestParam("postId") Long postId,
+            @RequestPart PostCommentReq postCommentReq,
+            @RequestHeader("X-AUTH-TOKEN") String requestAccessToken) {
         try{
             commentService.createComment(user, postId, postCommentReq);
             return new BaseResponse<>("댓글이 업로드되었습니다.");
@@ -56,14 +54,12 @@ public class CommentController {
      * @return BaseResponse<GetCommentsRes>
      */
     @ResponseBody
+    @NoAuthCheck
     @GetMapping("")
     @Operation(summary = "특정 게시물의 댓글 목록 조회", description = "특정 게시물의 댓글 목록을 조회합니다.")
-    public BaseResponse<List<GetCommentsRes>> getCommentsByPostId(@RequestParam("postId") Long postId,
-                                                                  @RequestHeader("X-AUTH-TOKEN") String requestAccessToken){
-        if (authService.validate(requestAccessToken)) { //유효한 사용자라 true가 반환됩니다 !!
-            return new BaseResponse<>(INVALID_JWT); //401 error : 유효한 사용자이지만, 토큰의 유효 기간이 만료됨.
-        }
-
+    public BaseResponse<List<GetCommentsRes>> getCommentsByPostId(
+            @RequestParam("postId") Long postId,
+            @RequestHeader("X-AUTH-TOKEN") String requestAccessToken) {
         try{
             List<GetCommentsRes> getCommentsRes = commentService.getCommentsByPostId(postId);
             return new BaseResponse<>(getCommentsRes);
@@ -78,15 +74,13 @@ public class CommentController {
      * @return BaseResponse<String>
      */
     @ResponseBody
+    @NoAuthCheck
     @DeleteMapping("/{commentId}")
     @Operation(summary = "댓글 삭제", description = "댓글을 삭제합니다.")
-    public BaseResponse<String> deleteComment(@AuthenticationPrincipal User user,
-                                              @PathVariable Long commentId,
-                                              @RequestHeader("X-AUTH-TOKEN") String requestAccessToken){
-        if (authService.validate(requestAccessToken)) { //유효한 사용자라 true가 반환됩니다 !!
-            return new BaseResponse<>(INVALID_JWT); //401 error : 유효한 사용자이지만, 토큰의 유효 기간이 만료됨.
-        }
-
+    public BaseResponse<String> deleteComment(
+            @AuthenticationPrincipal User user,
+            @PathVariable Long commentId,
+            @RequestHeader("X-AUTH-TOKEN") String requestAccessToken){
         try{
             commentService.deleteComment(user, commentId);
             return new BaseResponse<>("댓글이 삭제되었습니다.");
