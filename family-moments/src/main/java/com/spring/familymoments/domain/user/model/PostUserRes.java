20b4638c925561bc5ad651f81ccbd9a99fc36688
sonshn <sonshn@naver.com>
2024-01-04 20:36:10 +0900

    feat: user 관련 DTO에 swagger 적용

--- family-moments/src/main/java/com/spring/familymoments/domain/user/model/PostUserRes.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/user/model/PostUserRes.java
@@ -1,13 +1,18 @@
 package com.spring.familymoments.domain.user.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
 @NoArgsConstructor
+@Schema(description = "회원 가입 관련 Response")
 public class PostUserRes {
 
+    @Schema(description = "이메일", example = "family@gmail.com")
     private String email;
+    @Schema(description = "닉네임", example = "영희남편철수")
     private String nickname;
+    @Schema(description = "프로필 이미지")
     private String profileImg;
 
     public PostUserRes(String email, String nickname, String profileImg) {
