a770b57dc732e182d1380210a923d0806f1347fe
spacewalk00 <ju001217@gmail.com>
2024-01-28 21:38:17 +0900

    feat : 소셜 관련 API - swagger 적용

--- family-moments/src/main/java/com/spring/familymoments/domain/socialInfo/SocialUserController.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/socialInfo/SocialUserController.java
@@ -8,6 +8,13 @@ import com.spring.familymoments.domain.awsS3.AwsS3Service;
 import com.spring.familymoments.domain.socialInfo.model.*;
 import com.spring.familymoments.domain.user.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpCookie;
@@ -25,6 +32,7 @@ import static com.spring.familymoments.utils.ValidationRegex.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "SocialInfo", description = "네이버,카카오,구글 API Document")
 public class SocialUserController {
     private final SocialUserService socialUserService;
     private final UserService userService;
@@ -33,6 +41,10 @@ public class SocialUserController {
 
     @NoAuthCheck
     @PostMapping("/users/oauth2/social/login")
+    @Operation(summary = "소셜 로그인 (아이콘 클릭 시)", description = "기존 회원 대상 : 헤더로 토큰이 반환됩니다. / 신규 회원 대상(아래 예시 해당) : 사용자로부터 허용된 리소스 서버 정보들이 반환됩니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SocialLoginOrJoinResponse.JoinResponse.class)))
+    })
     public ResponseEntity<?> doSocialLogin(@Valid @RequestBody SocialLoginRequest request) throws Exception {
         SocialLoginOrJoinResponse socialLoginOrJoinResponse = socialUserService.doSocialLogin(request);
 
@@ -57,8 +69,12 @@ public class SocialUserController {
     }
     @NoAuthCheck
     @PostMapping("/users/oauth2/social/join")
-    public ResponseEntity<?> createSocialUser(@RequestPart("userJoinReq") UserJoinRequest userJoinRequest,
-                                                           @RequestPart("profileImg") MultipartFile profileImage) {
+    @Operation(summary = "소셜 회원가입과 로그인 (폼 입력)", description = "신규 회원 대상 : 얻은 리소스 서버 정보들과 사용자의 입력으로 유저를 등록하고 로그인(헤더로 토큰 발급)합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
+    public ResponseEntity<?> createSocialUser(@Parameter(description = "신규 회원의 가입 정보") @RequestPart("userJoinReq") UserJoinRequest userJoinRequest,
+                                              @Parameter(description = "신규 회원의 프로필 첨부파일") @RequestPart("profileImg") MultipartFile profileImage) {
         //아이디
         if (userJoinRequest.getId().isEmpty()) {
             return ResponseEntity.status(400)
--- family-moments/src/main/java/com/spring/familymoments/domain/socialInfo/model/SocialLoginRequest.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/socialInfo/model/SocialLoginRequest.java
@@ -1,5 +1,6 @@
 package com.spring.familymoments.domain.socialInfo.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +12,12 @@ import javax.validation.constraints.NotNull;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "소셜 서버(권한 부여 서버)에 인가코드(자격 증명)를 보내는 Request")
 public class SocialLoginRequest {
-    //사용자가 권한 부여 서버에게 자격 증명을 같이 보내는 단계
+    @Schema(description = "소셜 서버 종류", example = "KAKAO")
     @NotNull
     private String userType;
+    @Schema(description = "인가 코드", example = "yhgGywjpJXX8NaQJrgjN401U1VfW5__")
     @NotNull
     private String code;
 }
