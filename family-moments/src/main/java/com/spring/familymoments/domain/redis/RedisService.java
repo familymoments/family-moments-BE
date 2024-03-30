b9f7a8f86463ca20ca3e96a3b8ebf5ae9c62e427
zzo3ozz <wityoung.h@gmail.com>
2024-03-30 16:17:10 +0900

    feat: Redis - Set collection을 위한 저장/조회 method

--- family-moments/src/main/java/com/spring/familymoments/domain/redis/RedisService.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/redis/RedisService.java
@@ -5,6 +5,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -15,13 +16,23 @@ public class RedisService {
     public void setValues(String key, String value) {
         redisTemplate.opsForValue().set(key, value);
     }
+
+    public void setValues(String key, Set<Object> value) {
+        redisTemplate.opsForSet().add(key, value);
+    }
+
     @Transactional
     public void setValuesWithTimeout(String key, String value, long timeout) { // 만료 시간을 설정해서 자동 삭제 가능
         redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.MILLISECONDS);
     }
-    public String getValues(String key) {
+    public Object getValues(String key) {
         return redisTemplate.opsForValue().get(key);
     }
+
+    public Set<Object> getMembers(String key) {
+        return redisTemplate.opsForSet().members(key);
+    }
+
     public void deleteValues(String key) {
         redisTemplate.delete(key);
     }
