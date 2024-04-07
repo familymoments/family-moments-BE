e1e08449323b6c234d732ee65f9902ba8f492b46
serak0310 <serak0310@naver.com>
2024-04-08 00:49:58 +0900

    fix: 불필요한 import 삭제

--- family-moments/src/main/java/com/spring/familymoments/domain/fcm/FCMScheduler.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/fcm/FCMScheduler.java
@@ -3,7 +3,6 @@ package com.spring.familymoments.domain.fcm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @Slf4j
 @Configuration
