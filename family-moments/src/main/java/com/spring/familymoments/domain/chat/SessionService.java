4ff4de682f21fe635981c137ea76b36259131522
zzo3ozz <wityoung.h@gmail.com>
2024-05-31 13:26:31 +0900

    feat: disconnect 시 sub 중인 가족 채팅에 대한 lastAccessedTime 갱신

--- family-moments/src/main/java/com/spring/familymoments/domain/chat/SessionService.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/chat/SessionService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import static com.spring.familymoments.config.BaseResponseStatus.minnie_FAMILY_INVALID_USER;
 import static com.spring.familymoments.domain.chat.ChatRedisPrefix.*;
@@ -38,10 +39,10 @@ public class SessionService {
         for(UserFamily userFamily : userFamilyList) {
             Family family = userFamily.getFamilyId();
 
-            // offline에서 제거
+            // offline 세션에서 제거
             redisService.removeMember(FAMILY_OFF.value + family.getFamilyId(), user.getUuid());
 
-            // unsub 상태로 변경
+            // unsub 세션에 추가
             redisService.addValues(FAMILY_UNSUB.value + family.getFamilyId(), user.getUuid());
         }
     }
@@ -55,7 +56,18 @@ public class SessionService {
         List<UserFamily> userFamilyList = userFamilyRepository.findUserFamilyByUserId(user.getUserId());
 
         for(UserFamily userFamily : userFamilyList) {
+            Set<String> members = redisService.getMembers(FAMILY_UNSUB.value + userFamily.getFamilyId());
+            boolean isSubscribing = !members.contains(uuid);
+
+            // unsub 세션에서 제거
             redisService.removeMember(FAMILY_UNSUB.value + userFamily.getFamilyId(), uuid);
+
+            // sub 중인 채팅방이 있다면 unsub처리 (lastAccessedTime 갱신을 위함)
+            if(isSubscribing) {
+                unsubscribeFamily(userFamily);
+            }
+
+            // offline 세션에 추가
             redisService.addValues(FAMILY_OFF.value + userFamily.getFamilyId(), uuid);
         }
 
@@ -70,6 +82,7 @@ public class SessionService {
         Family family = familyRepository.findById(familyId).orElseThrow(() -> new BaseException(BaseResponseStatus.FIND_FAIL_FAMILY));
         UserFamily userFamily = userFamilyRepository.findByUserIdAndFamilyId(user, family).orElseThrow(() -> new BaseException(BaseResponseStatus.minnie_FAMILY_INVALID_USER));
 
+        // unsub 세션에서 제거
         redisService.removeMember(FAMILY_UNSUB.value + familyId, user.getUuid());
     }
 
@@ -78,9 +91,16 @@ public class SessionService {
         Family family = familyRepository.findById(familyId).orElseThrow(() -> new BaseException(BaseResponseStatus.FIND_FAIL_FAMILY));
         UserFamily userFamily = userFamilyRepository.findByUserIdAndFamilyId(user, family).orElseThrow(() -> new BaseException(BaseResponseStatus.minnie_FAMILY_INVALID_USER));
         
-        // TODO: 마지막 접속 시간 갱신
-        
-        redisService.addValues(FAMILY_UNSUB.value + familyId, user.getUuid());
+        unsubscribeFamily(userFamily);
+    }
+
+    // 가족 방 구독 해제
+    public void unsubscribeFamily(UserFamily userFamily) {
+        // 마지막 접속 시간 갱신
+        userFamily.updateLastAccessedTime(LocalDateTime.now());
+
+        // unsub 세션에 추가
+        redisService.addValues(FAMILY_UNSUB.value + userFamily.getFamilyId().getFamilyId(), userFamily.getUserId().getUuid());
     }
 
     // 접속한 유저의 세션 정보 저장
