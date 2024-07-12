41e92b9618e3e8b8a50399210e1bedea10dbc2b3
zzo3ozz <wityoung.h@gmail.com>
2024-07-12 16:25:03 +0900

    refactor: S3 Controller - delete method 사용 가능하도록 수정, 및 NoAuthCheck 처리

--- family-moments/src/main/java/com/spring/familymoments/domain/awsS3/AwsS3Controller.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/awsS3/AwsS3Controller.java
@@ -2,9 +2,9 @@ package com.spring.familymoments.domain.awsS3;
 
 import com.spring.familymoments.config.BaseException;
 import com.spring.familymoments.config.BaseResponse;
+import com.spring.familymoments.config.NoAuthCheck;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,6 +27,7 @@ public class AwsS3Controller {
     @ResponseBody
     @PostMapping("/image")
     @Operation(summary = "이미지 등록", description = "이미지 한 개를 S3에 등록하고 URL을 반환합니다.")
+    @NoAuthCheck
     public BaseResponse<String> uploadImage(@RequestParam(name = "image") MultipartFile image) throws BaseException {
         String fileUrl = awsS3Service.uploadImage(image);
         return new BaseResponse<>(fileUrl);
@@ -35,6 +36,7 @@ public class AwsS3Controller {
     @ResponseBody
     @PostMapping("/images")
     @Operation(summary = "다중 이미지 등록", description = "이미지 여러 개를 S3에 등록하고 URL을 반환합니다.")
+    @NoAuthCheck
     public BaseResponse<List<String>> uploadImages(@RequestPart(required = false) List<MultipartFile> images) throws BaseException {
         List<String> fileUrls = awsS3Service.uploadImages(images);
         return new BaseResponse<>(fileUrls);
@@ -43,7 +45,8 @@ public class AwsS3Controller {
     @ResponseBody
     @DeleteMapping("")
     @Operation(summary = "이미지 삭제", description = "이미지를 S3에서 삭제합니다.")
-    public BaseResponse<String> deleteImage(String fileName) {
+    @NoAuthCheck
+    public BaseResponse<String> deleteImage(@RequestParam(name = "url", required = true) String fileName) {
         try {
             awsS3Service.deleteImage(fileName);
             return new BaseResponse<>("성공했습니다.");
