00348b3ac7a868c4c99f289bbf2f04c1a3f3bacd
sonshn <sonshn@naver.com>
2024-09-09 06:45:18 +0900

    refactor: MultiPostRes 삭제

--- family-moments/src/test/java/com/spring/familymoments/PostServiceTest.java
+++ family-moments/src/test/java/com/spring/familymoments/PostServiceTest.java
@@ -1,16 +1,6 @@
 package com.spring.familymoments;
 
-import com.spring.familymoments.config.BaseException;
-import com.spring.familymoments.domain.post.PostRepository;
-import com.spring.familymoments.domain.post.PostService;
-import com.spring.familymoments.domain.post.model.AlbumRes;
-import com.spring.familymoments.domain.post.model.MultiPostRes;
-import com.spring.familymoments.domain.post.model.SinglePostRes;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 @SpringBootTest
 public class PostServiceTest {
